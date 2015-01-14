86c95cd23fd240f753325dbaf96cda1fe2448838
Danny Sheehan <danny@ftmon.org>
2015-01-14 15:21:42 +1000

    added syslog logging and email notification. Also example whitelist.txt

--- README.md
+++ README.md
@@ -24,10 +24,17 @@ So we recommending using **cpulimit** to invoke blacklists.sh
     -  /var/lib/blacklists/{whitelist.txt,blacklist.txt}
 - run _sudo blacklists.sh_  
 - setup your _/etc/crontab_
-~~~
-@reboot         root    /usr/local/bin/blacklists.sh
-@daily          root    /usr/local/bin/blacklists.sh
-~~~
+    ~~~
+    @reboot         root    /usr/local/bin/blacklists.sh
+    @daily          root    /usr/local/bin/blacklists.sh
+    ~~~
+- NOTE: If your hosting provider is Ramnode your terms of service prevent you from using all
+your available CPU load.  So use cpulimit to restrict the CPU usage to 20%.
+    ~~~
+    @reboot         root    cpulimit -z -l 20 /usr/local/bin/blacklists.sh
+    @daily          root    cpulimit -z -l 20 /usr/local/bin/blacklists.sh
+    ~~~
+
 - setup logging
 
 _/etc/logrotate.d/blacklist_
@@ -70,8 +77,68 @@ ports are blocked without logging.
 This prevents blacklist providers banning your IP for downloading too often.
 - uses a temporary ipset when loading updated blacklists to ensure you are always protected
 during blacklist updates.
+- after a reboot cached ipsets are loaded to ensure you are protected faster after an outage and not left exposed until the blacklists are re-imported.
+
+## Example syslog messages
+
+- everything is logged to syslog, for your monitoring to pick up issues.
+
+~~~
+Jan 14 14:51:32 serverx [/usr/local/bin/blacklists.sh]: ftmon.org blacklist script started
+Jan 14 14:52:17 serverx [/usr/local/bin/blacklists.sh]: ERROR Your whitelist IP 54.235.163.229 has been blacklisted in lists-blocklist-de-all
+Jan 14 14:57:47 serverx [/usr/local/bin/blacklists.sh]: ERROR Your whitelist IP 67.207.202.9 has been blacklisted in infiltrated.net
+Jan 14 15:02:03 serverx [/usr/local/bin/blacklists.sh]: bad_ips: current=53435   previous=53435   bad_nets: previous=1535   current=1535
+Jan 14 15:02:03 serverx [/usr/local/bin/blacklists.sh]: ftmon.org blacklist script completed
+~~~
+
+### Example email message
+
+- optional feature to be emailed if there are issues.
+
+~~~
+From: root
+Date: Wed, Jan 1, 2015 at 3:09 PM
+Subject: [/usr/local/bin/blacklists.sh] sever.org
+To: root
 
 
+bad_ips: current=29294   previous=57196   bad_nets: previous=1536   current=1536
+
+ERROR Your whitelist IP 192.0.81.17 has been blacklisted in lists-blocklist-de-all
+ERROR Your whitelist IP 192.0.81.57 has been blacklisted in lists-blocklist-de-all
+ERROR Your whitelist IP 67.207.202.9 has been blacklisted in infiltrated.net
+~~~
+
+
+### Firewall audit log of production ports
+
+- only ports such as DNS,HTTP,HTTPS are logged, so you can
+go back and do auditing in case legitimate traffic is being blocked.
+
+_/var/log/blacklists.log_
+~~~
+Jan 1 19:24:42 server kernel: [541334.229673] [BL DROP] IN=eth0 OUT= MAC=d4:be:d9:a1:62:06:78:da:6e:25:cc:00:08:00 SRC=124.232.142.220 DST=x.x.x.x LEN=58 TOS=0x00 PREC=0x00 TTL=234 ID=54321 PROTO=UDP SPT=47479 DPT=53 LEN=38
+~~~
+
+### Firewall rules created
+
+- this is the iptables chain that is automatically created based on `TCP_PORTS="53,80,443"`
+and `UDP_PORTS="53"`
+- production ports are rejected not droped so as not to "stir up" hackers.
+- there is also rate limiting on production port logging.
+
+
+~~~
+iptables -L ftmon-blacklists
+Chain ftmon-blacklists (2 references)
+target     prot opt source               destination
+LOG        tcp  --  anywhere             anywhere             multiport dports http,https limit: avg 5/min burst 5 LOG level warning prefix "[BL DROP] "
+LOG        udp  --  anywhere             anywhere             multiport dports domain limit: avg 5/min burst 5 LOG level warning prefix "[BL DROP] "
+REJECT     tcp  --  anywhere             anywhere             state NEW multiport dports http,https reject-with icmp-port-unreachable
+REJECT     udp  --  anywhere             anywhere             state NEW multiport dports domain reject-with icmp-port-unreachable
+DROP       all  --  anywhere             anywhere             state NEW
+
+~~~
 
 ## References and Other blacklist scripts
 
--- blacklists.sh
+++ blacklists.sh
@@ -8,54 +8,87 @@
 #website        :www.ftmon.org  www.setuptips.com
 #---------------------------------------------------------------------------
 
-# Some hosting services such as RamNode will ban you for using > 90% of the cpu.
-# So we recommend installing cpulimit and limiting to 20% of cpu usage.
-## cpulimit -l 20 /usr/local/bin/blacklists.sh
-
-
-# Retrieve new blacklists when they are older then BL_AGE
-BL_AGE="23 hours ago"
-
-# We we keep all the blacklists.
+#
+# Where we keep all the blacklists.
 BL_DIR="/var/lib/blacklists"
+mkdir -p $BL_DIR
 
-# Logging is enabled for the following ports, so we can check (audit)
-# if are droping legitimate traffic.
+# Some hosting services such as RamNode will ban you for using > 90% of the cpu!!!
+# So we recommend installing cpulimit and limiting to 20% of cpu usage when 
+# calling this script.
+#
+## cpulimit -z -l 20 /usr/local/bin/blacklists.sh
+# cpulimit dosn't like scripts writing to stdout/stderr so them redirect to 
+# an output file.
+#
+exec > $BL_DIR/blacklists.out 2>&1
+
+SCRIPT_NAME=$0
+HOST_NAME=`uname -n`
+
+# default syslog messages priority and tag.
+LOG_PRI="local0.notice"
+LOG_TAG="[$SCRIPT_NAME]"
+
+# Set to empty string if you don't want error emails. Otherwise, set to an admin email.
+MAIL_ADMIN="root"
+
+# Logging is enabled for the following ports this is so we can do later audit checks
+# in case we are droping legitimate traffic.
 TCP_PORTS="53,80,443"
 UDP_PORTS="53"
 
 # If PSAD is installed then block Danger Level = $DL and above attackers
-# each day the blacklists are reloaded.
+# each time the blacklists are reloaded.
 DL=3
 
-# goodinbadnets 
+# Retrieve new blacklists only when they are older then BL_AGE
+BL_AGE="23 hours ago"
+
+#---------------------------------------------------------------------------
+
+# logmessage <msg_text>
+logmessage () {
+  MSG="$1"
+  logger -s -p $LOG_PRI -t $LOG_TAG "$MSG"
+}
+
+# <ips> goodinbadnets
+# - returns whitelist <ips> that that are in blacklists.
 goodinbadnets () {
   myips=""
   for good in `ipset list good_ips | egrep -E "^[1-9]"`
   do
    myip=`ipset test bad_nets_n $good 2>&1 | grep "is in" | awk '{print $1}'`
-   if [[ -n $myip ]];then
+   if [ -n "$myip" ];then
      myips="$myips $myip"
    fi
   done
   echo $myips
 }
 
-#blaclkist <ip/cdr> <listname>
+# blacklistit <ip/cdr> <listname>
+#  - blacklists the given <ip/cdr> to bad_nets_n or gad_ips_n
+#  - also checks if the <ip/cdr> blacklists one of your whitelisted ips, and 
+#  if so it will remove it from the blacklist and warn you.
 blacklistit () {
  IP=$1
  LISTNAME=$2
  if echo "$IP" | egrep -q "\/[0-9]+"; then
    ipset add bad_nets_n $IP -exist
    badip=`goodinbadnets`
-   if [[ -n "$badip" ]] ; then
-     echo "*** Your whitelist IP $badip has been blacklisted in $LISTNAME" >&2
+   if [ -n "$badip" ]; then
+     error_msg="ERROR Your whitelist IP $badip has been blacklisted in $LISTNAME"
+     logmessage "$error_msg"
+     ERROR_MSGS="$ERROR_MSGS\n$error_msg"
      ipset del bad_nets_n $IP
    fi
         
  else
    if ipset test good_ips $IP 2> /dev/null; then
-     echo "*** Your whitelist IP $IP has been blacklisted in $LISTNAME" >&2
+     error_msg="ERROR Your whitelist IP $IP has been blacklisted in $LISTNAME"
+     logmessage "$error_msg"
+     ERROR_MSGS="$ERROR_MSGS\n$error_msg"
    else 
      ipset add bad_ips_n $IP -exist
    fi
@@ -63,24 +96,23 @@ blacklistit () {
 }
 
 # loadblacklist <name> <url>
+# - loads standard form blacklist from <url> website, labels cache files with <name>
 loadblacklist () {
   BL_NAME=$1
   BL_URL=$2
 
   BL_FILE="$BL_DIR/$BL_NAME.txt"
-  if [[ ! -f "$BL_FILE" || \
-       $(date +%s -r $BL_FILE) -lt $(date +%s --date="$BL_AGE") ]]
-  then
-    echo "-- getting fresh $BL_NAME from $BL_URL" >&2
+  if [ ! -f "$BL_FILE" ] || [ $(date +%s -r "$BL_FILE") -lt $(date +%s --date="$BL_AGE") ]; then
+    echo "-- getting fresh $BL_NAME from $BL_URL"
     wget -q -t 2 --output-document=$BL_FILE $BL_URL
   fi
   
-  if [[ -f $BL_FILE ]]; then
-    echo "-- loading $BL_NAME from $BL_FILE" >&2
+  if [ -f "$BL_FILE" ]; then
+    echo "-- loading $BL_NAME from $BL_FILE"
 
     # strip comments - mac address and ipv6 not supported yet so strip :
     awk '{print $1}' $BL_FILE | cut -d\; -f1 | cut -d\, -f1 | grep -Ev "^#|^ *$|:" | sed -e "s/[^0-9\.\/]//g" | grep -E "^[0-9]" > ${BL_FILE}.filtered
-    echo "-- loading $BL_NAME - `wc -l ${BL_FILE}.filtered` entries" >&2
+    echo "-- loading $BL_NAME - `wc -l ${BL_FILE}.filtered` entries"
 
     for ip in `cat ${BL_FILE}.filtered`; do
       blacklistit $ip $BL_NAME
@@ -88,17 +120,21 @@ loadblacklist () {
   fi
 }
 
+#---------------------------------------------------------------------------
+# MAIN
+#---------------------------------------------------------------------------
 
-mkdir -p $BL_DIR
+# concatenated list of all error message
+ERROR_MSGS=""
+
 
 if ! which ipset > /dev/null 2>&1;then
-  echo "ERROR: You must install 'ipset'" >&2
+  echo "ERROR: You must install 'ipset'"
   exit 1
 fi
 
 
-echo "ftmon.org blacklister -"  >&2
-echo >&2
+logmessage "ftmon.org blacklist script started"
 
 
 # Create temporary swap ipsets
@@ -110,25 +146,25 @@ ipset flush bad_nets_n
 
 #
 # Setup the active ipsets if they don't yet exist.
+# Load them from last save sets to speed up load times in cases of reboot
+# and ensure protection faster.
 #
 if ! ipset list bad_ips > /dev/null 2>&1
 then
-  echo "-- creating bad_ips ipset as does not exist." >&2
+  echo "-- creating bad_ips ipset as does not exist."
   ipset create bad_ips hash:ip hashsize 4096 maxelem 262144
-  if [[ -f "$BL_DIR/bad_ips.sav" ]]
-  then
-    echo "-- importing from save file $BL_DIR/bad_ips.sav" >&2
+  if [ -f "$BL_DIR/bad_ips.sav" ]; then
+    echo "-- importing from save file $BL_DIR/bad_ips.sav"
     grep -v "create" $BL_DIR/bad_ips.sav | ipset restore 
   fi
 fi
 
 if ! ipset list bad_nets > /dev/null 2>&1
 then
-  echo "-- creating bad_nets ipset as does not exist." >&2
+  echo "-- creating bad_nets ipset as does not exist."
   ipset create bad_nets hash:net hashsize 4096 maxelem 262144
-  if [[ -f "$BL_DIR/bad_nets.sav" ]]
-  then
-    echo "-- importing from save file $BL_DIR/bad_nets.sav" >&2
+  if [ -f "$BL_DIR/bad_nets.sav" ]; then
+    echo "-- importing from save file $BL_DIR/bad_nets.sav"
     grep -v "create" $BL_DIR/bad_nets.sav | ipset restore 
   fi
 fi
@@ -138,7 +174,7 @@ fi
 #
 if ! iptables -L ftmon-blacklists -n > /dev/null 2>&1; then
 
-  echo "-- creating iptables rules for first time" >&2
+  echo "-- creating iptables rules for first time"
   iptables -N ftmon-blacklists
 
   iptables -I INPUT  \
@@ -165,76 +201,68 @@ if ! iptables -L ftmon-blacklists -n > /dev/null 2>&1; then
 fi
 
 
-
-
-#List of ips to whitelist
+# List of ips to whitelist
 if ! ipset list good_ips > /dev/null 2>&1; then
   ipset create good_ips hash:ip
 fi
 
-# load fresh list each time as the list should be small.
+# load fresh white list each time as the list should be small.
 ipset flush good_ips
 
 # load your good ip's
 WL_CUSTOM="$BL_DIR/whitelist.txt"
 count=0
-if [[ -f "$WL_CUSTOM" ]]; then
+if [ -f "$WL_CUSTOM" ]; then
   for ip in `grep -Ev "^#|^ *$" $WL_CUSTOM | sed -e "s/#.*$//" -e "s/[^.0-9\/]//g"`; do
      ipset add good_ips $ip -exist
      count=$((count+1))
   done
 fi
-echo "-- loaded $count entries from $WL_CUSTOM" >&2
+echo "-- loaded $count entries from $WL_CUSTOM"
 
 # load your personal custom blacklists.
 BL_CUSTOM="$BL_DIR/blacklist.txt"
 count=0
-if [[ -f "$BL_CUSTOM" ]]; then
+if [ -f "$BL_CUSTOM" ]; then
   for ip in `grep -Ev "^#|^ *$" $BL_CUSTOM | sed -e "s/#.*$//" -e "s/[^.0-9\/]//g"`; do
     blacklistit $ip $BLACKLIST
     count=$((count+1))
   done
 fi
-echo "-- loaded `ipset list bad_ips_n | egrep "^[1-9]"  | wc -l` entries from blacklist " >&2
-echo "-- loaded $count entries from $BL_CUSTOM" >&2
+echo "-- loaded `ipset list bad_ips_n | egrep "^[1-9]"  | wc -l` entries from blacklist "
+echo "-- loaded $count entries from $BL_CUSTOM"
 
 # If PSAD is installed then use some of it's good detection work
 # to stop attackers.
 count=0
-if [[ -f "/var/log/psad/top_attackers" ]];then
+if [ -f "/var/log/psad/top_attackers" ]; then
  for ip in `awk '{print $2, $1}' /var/log/psad/top_attackers | grep "^[$DL-]" | awk '{print $2}'`; do
     blacklistit $ip $BLACKLIST
     count=$((count+1))
   done
 fi
-echo "-- loaded $count entries from /var/log/psad/top_attackers " >&2
+echo "-- loaded $count entries from /var/log/psad/top_attackers "
 
 #
 # Load Standard format blacklists
+# Some of them are over zealous, you may want to comment out.
 #
-
 loadblacklist \
   "lists-blocklist-de-all" \
   "http://lists.blocklist.de/lists/all.txt"
 
-# http://ipsec.pl/files/ipsec/blacklist-ip.txt
 loadblacklist \
    "ipsec-pl" \
    "http://doc.emergingthreats.net/pub/Main/RussianBusinessNetwork/RussianBusinessNetworkIPs.txt"
 
-
-# http://www.infiltrated.net/blacklisted
 loadblacklist \
    "infiltrated.net" \
    "http://www.infiltrated.net/blacklisted"
 
-# Obtain List from openbl.org
-#http://www.openbl.org/lists.html
 loadblacklist \
   "openbl-org-base" \
   "http://www.openbl.org/lists/base.txt"
 
-# 
 loadblacklist \
       "ci-army-malcious" \
         "http://cinsscore.com/list/ci-badguys.txt"
@@ -251,7 +279,6 @@ loadblacklist \
       "torexitnodes" \
         "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1"
 
-#
 loadblacklist \
       "spamhaus-org-lasso" \
         "http://www.spamhaus.org/drop/drop.lasso"
@@ -280,7 +307,7 @@ loadblacklist \
 
 
 #
-# special cases, custom formats
+# special cases, custom formats blacklists
 #
 
 # Obtain List of badguys from dshield.org
@@ -289,15 +316,13 @@ loadblacklist \
   BL_URL="http://feeds.dshield.org/top10-2.txt"
 
   BL_FILE="$BL_DIR/$BL_NAME.txt"
-  if [[ ! -f "$BL_FILE" || \
-       $(date +%s -r $BL_FILE) -lt $(date +%s --date="$BL_AGE") ]]
-  then
-    echo "-- getting fresh $BL_NAME from $BL_URL" >&2
+  if [ ! -f "$BL_FILE" ] || [ $(date +%s -r "$BL_FILE") -lt $(date +%s --date="$BL_AGE") ]; then
+    echo "-- getting fresh $BL_NAME from $BL_URL"
     wget -q -t 2 --output-document=$BL_FILE $BL_URL
   fi
   
-  if [[ -f $BL_FILE ]]; then
-    echo "-- loading $BL_NAME from $BL_FILE" >&2
+  if [ -f "$BL_FILE" ]; then
+    echo "-- loading $BL_NAME from $BL_FILE"
     for ip in `grep -E "^[1-9]" $BL_FILE | cut -f1`; do
       blacklistit $ip $BL_NAME
     done
@@ -310,12 +335,17 @@ ipset swap bad_ips_n bad_ips
 ipset swap bad_nets_n bad_nets
 
 # show before and after counts.
-echo "-- previous `ipset --list bad_ips_n | egrep "^[1-9]" | wc -l` bad_ips" >&2
-echo "-- loaded `ipset --list bad_ips  | egrep "^[1-9]" | wc -l` bad_ips" >&2
+complete_msg="bad_ips: current=`ipset --list bad_ips_n | egrep '^[1-9]' | wc -l` \
+  previous=`ipset --list bad_ips  | egrep '^[1-9]' | wc -l` \
+  bad_nets: previous=`ipset --list bad_nets | egrep '^[1-9]' | wc -l` \
+  current=`ipset --list bad_nets_n | egrep '^[1-9]' | wc -l`"
 
-echo >&2
-echo "-- loaded `ipset --list bad_nets | egrep "^[1-9]" | wc -l` bad_nets" >&2
-echo "-- previous `ipset --list bad_nets_n | egrep "^[1-9]" | wc -l` bad_nets" >&2
+logmessage "$complete_msg"
+
+# only send email if problems.
+if [ -n "$MAIL_ADMIN" ] && [ -n "$ERROR_MSGS" ]; then
+  echo -e "${complete_msg}\n${ERROR_MSGS}" | mail -s "$LOG_TAG $HOST_NAME" $MAIL_ADMIN
+fi
 
 
 # save memory space by destroying the temporary swap ipset
@@ -326,3 +356,6 @@ ipset destroy bad_nets_n
 # save our ipsets for quick import on reboot.
 ipset save bad_ips  > $BL_DIR/bad_ips.sav
 ipset save bad_nets > $BL_DIR/bad_nets.sav
+
+logmessage "ftmon.org blacklist script completed"
+
